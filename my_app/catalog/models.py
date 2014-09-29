037a16968d511e3b4ae10ce7f7099f896ddc8d57
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-09-30 01:52:04 +0530

    Chapter 5 - Recipe 'Creating custom field and validation'

--- my_app/catalog/models.py
+++ my_app/catalog/models.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from wtforms import TextField, DecimalField, SelectField
 from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired, NumberRange
+from wtforms.validators import InputRequired, NumberRange, ValidationError
 from my_app import db
 
 class Product(db.Model):
@@ -37,14 +37,48 @@ class NameForm(FlaskForm):
     name = TextField('Name', validators=[InputRequired()])
 
 
+class CategoryField(SelectField):
+
+    def iter_choices(self):
+        categories = [(c.id, c.name) for c in Category.query.all()]
+        for value, label in categories:
+            yield (value, label, self.coerce(value) == self.data)
+
+    def pre_validate(self, form):
+        for v, _ in [(c.id, c.name) for c in Category.query.all()]:
+            if self.data == v:
+                break
+        else:
+            raise ValueError(self.gettext('Not a valid choice'))
+
+
 class ProductForm(NameForm):
     price = DecimalField('Price', validators=[
         InputRequired(), NumberRange(min=Decimal('0.0'))
     ])
-    category = SelectField(
+    category = CategoryField(
         'Category', validators=[InputRequired()], coerce=int
     )
 
 
+def check_duplicate_category(case_sensitive=True):
+    def _check_duplicate(form, field):
+        if case_sensitive:
+            res = Category.query.filter(
+                Category.name.like('%' + field.data + '%')
+            ).first()
+        else:
+            res = Category.query.filter(
+                Category.name.ilike('%' + field.data + '%')
+            ).first()
+        if res:
+            raise ValidationError(
+                'Category named %s already exists' % field.data
+            )
+    return _check_duplicate
+
+
 class CategoryForm(NameForm):
-    pass
+    name = TextField('Name', validators=[
+        InputRequired(), check_duplicate_category()
+    ])
